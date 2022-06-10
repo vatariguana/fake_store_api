c169456218104cc77cdeec0adcbc9a382e4c3191
Veronica Atariguana <vatariguanag@gmail.com>
2022-06-10 17:16:49 -0500

    counter product detail

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import "./index.scss";
 const Counter = () => {
-  return <div>hola desde counter</div>;
+  const [counter, setCounter] = useState(1);
+  const handleDecrement = () => {
+    setCounter(counter - 1);
+  };
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+  return (
+    <div className="counter-container">
+      <button
+        className="decrement"
+        onClick={handleDecrement}
+        disabled={counter === 1}
+      >
+        -
+      </button>
+      <div className="counter">{counter}</div>
+      <button className="increment" onClick={handleIncrement}>
+        +
+      </button>
+    </div>
+  );
 };
 export default Counter;
